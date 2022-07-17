ec4ceeac8579f72ba5caa238b2128b673b8e0899
CM_WIN_LAPTOP\Caspian <caspian@merlins.online>
2022-07-18 00:07:33 +0100

    Added world coordinates

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use sdl2::image::{InitFlag, LoadTexture};
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2::render::WindowCanvas;
+use sdl2::rect::{Rect, Point};
+use sdl2::render::{WindowCanvas, Texture};
 use std::thread;
 use std::time::Duration;
 
@@ -10,12 +12,18 @@ pub fn main() -> Result<(), String> {
     // Initialisation
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
+    let _image_context = sdl2::image::init(InitFlag::PNG | InitFlag::JPG)?;
     let window = video_subsystem.window("GUI Playground", 1024, 768)
         .position_centered()
         .build()
-        .expect("Unable to create window.");
+        .map_err(|e| e.to_string())?;
 
-    let mut canvas = window.into_canvas().build().expect("Unable to build canvas.");
+    let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
+    let texture_creator = canvas.texture_creator();
+    let texture = texture_creator.load_texture("assets/bardo.png")?;
+    let position = Point::new(0, 0);
+    let sprite = Rect::new(0, 0, 26, 36);
+
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
 
@@ -38,7 +46,7 @@ pub fn main() -> Result<(), String> {
         i = (i + 1) % 255;
 
         // Render
-        render(&mut canvas, Color::RGB(i, 64, 255 - i));
+        render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture, position, sprite)?;
         
         // Time management
         thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
@@ -49,8 +57,18 @@ pub fn main() -> Result<(), String> {
 
 
 
-fn render(canvas: &mut WindowCanvas, colour: Color) {
+fn render(canvas: &mut WindowCanvas, colour: Color, texture: &Texture, position: Point, sprite: Rect) -> Result<(), String> {
+
+    //Calculating position
+    let (width, height) = canvas.output_size()?;
+    let screen_position = position + Point::new(width as i32 / 2, height as i32 / 2);
+    let screen_rect = Rect::from_center(screen_position, sprite.width(), sprite.height());
+
+    // Rendering
     canvas.set_draw_color(colour);
     canvas.clear();
+    canvas.copy(texture, sprite, screen_rect)?;
+
     canvas.present();
+    Ok(())
 }
